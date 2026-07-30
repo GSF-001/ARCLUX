4514e74c215523facd05b949c8a142ec81f8c1ca
GSF-001 <230616882+GSF-001@users.noreply.github.com>
2026-07-31 06:26:05 +0700

    refactor(web): consolidate ui primitives into components/ui, remove duplicates from primitives

---
+++ apps/web/components/ui/button.tsx
@@ -0,0 +1,2 @@
+export { Button, buttonVariants } from "@/vendor-ui/shadcn/button"
+export type { ButtonProps } from "@/vendor-ui/shadcn/button"
---
+++ apps/web/components/ui/dialog.tsx
@@ -0,0 +1,1 @@
+export * from "@/vendor-ui/shadcn/dialog"
